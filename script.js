1b7a11c16433467d3eafcba6d342b41aaa24236d
Alexandre Saints <73872086+alexandresaints@users.noreply.github.com>
2021-08-10 03:11:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function abrir(){
+
+    document.getElementById('popup').style.display = 'block';
+
+}
+
+function fechar(){
+
+    document.getElementById('popup').style.display = 'none';
+
+}
+
+function abrirMobile(){
+
+    document.getElementById('popup-mobile').style.display = 'block';
+
+}
+
+function fecharMobile(){
+
+    document.getElementById('popup-mobile').style.display = 'none';
+
+}
